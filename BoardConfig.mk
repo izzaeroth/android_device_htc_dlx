a4df5f263e67c17745e23fc6ff650b897c5a85ea
izzaeroth <izzaeroth@gmail.com>
2015-05-20 13:59:18 -0500

    add custom toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,10 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/dlx/include
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
+# Custom toolchains
+TARGET_GCC_VERSION_ARM := arm-eabi-4.9-ln
+TARGET_KERNEL_TC_SUFFIX := arm-cortex_a15-linux-gnueabihf-
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := dlx
 
