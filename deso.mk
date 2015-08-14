76ad05ac3c8dd6a27c1ed7f7d4720a39c59288c5
izzaeroth <izzaeroth@gmail.com>
2015-08-14 16:30:22 -0500

    update hardware info

--- deso.mk
+++ deso.mk
@@ -27,6 +27,15 @@ PRODUCT_BRAND := Verizon
 PRODUCT_MODEL := HTC6435LVW
 PRODUCT_MANUFACTURER := HTC
 
+# Device hardware info
+PRODUCT_CHIPSET := Qualcomm MDM615m/APQ8064
+PRODUCT_CPU := Quad-core 1.5 GHz Krait 300
+PRODUCT_GPU := Adreno 320
+PRODUCT_REAR_CAM := 8 MP, 3264 x 2448 pixels
+PRODUCT_FRONT_CAM := 2.1 MP
+PRODUCT_SCREEN_SIZE := 5.0 inches ~441 ppi, Super LCD3
+PRODUCT_SCREEN_RES := 1080 x 1920 pixels
+
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dlx \
